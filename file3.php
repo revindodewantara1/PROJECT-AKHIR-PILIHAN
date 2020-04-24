ae04d4b7264ef20c55b98477c5df43746feedabb
revindodewantara1 <63893659+revindodewantara1@users.noreply.github.com>
2020-04-24 13:39:25 +0700

    Add files via upload

---
+++ file3.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>File</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+<div class="quest-box3">
+<img src="avatar2.png" class="avatar">
+<h2>4.Apakah anda pernah muncul gejala sekitar 14 hari setelah travelling ke luar
+negeri? (China,italy,iran,Korea selatan,Prancis,Spanyol,Jerman,USA)
+atau ke kota terjangkit (Jakarta,Bali,Solo,Yogyakarta,Pontianak,Manado,Bandung,DLL</h2>
+
+</h5>
+<table>
+    <td> 
+        <tr> 
+        <form method="get" action="./file4.php">
+    <button class="tomboltidak" type="submit" href="file4.php">Tidak</button>
+</form>
+        </tr>
+    </td>
+    <td> 
+        <tr> 
+ 
+        <form method="get" action="./peringatan.php">
+    <button class="tomboliya2" type="submit" href="peringatan.php">Iya</button>
+        </tr>
+    </td>
+</table>
+
+</div>
+
+</form>
+</body>
+</html>
