5ddcd3e3eae51a1c8804be6d82e5eecf8f545754
revindodewantara1 <63893659+revindodewantara1@users.noreply.github.com>
2020-04-24 13:38:41 +0700

    Add files via upload

---
+++ file5.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>File</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+<div class="quest-box5">
+<img src="avatar2.png" class="avatar">
+<h2>6.Apakah anda memiliki kontak dekat dengan seseorang yang berpergian ke luar negeri dalam
+14 hari terakhir yang menjadi sakit(batuk,demam,bersin, atau sakit tenggorokan)?</h2>
+
+</h5>
+<table>
+    <td> 
+        <tr> 
+        <form method="get" action="./kesimpulan.php">
+    <button class="tomboltidak" type="submit" href="kesimpulan.php">Tidak</button>
+</form>
+        </tr>
+    </td>
+    <td> 
+        <tr> 
+ 
+        <form method="get" action="./peringatan.php">
+    <button class="tomboliya3" type="submit" href="peringatan.php">Iya</button>
+        </tr>
+    </td>
+</table>
+</div>
+
+</form>
+</body>
+</html>
