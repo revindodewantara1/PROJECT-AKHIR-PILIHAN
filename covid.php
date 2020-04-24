e88d5a05917f80b2ce642785879f42acb95c85f7
revindodewantara1 <63893659+revindodewantara1@users.noreply.github.com>
2020-04-24 13:40:36 +0700

    Add files via upload

---
+++ covid.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>File</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+<div class="quest-box">
+<img src="avatar2.png" class="avatar">
+<h2>1.Apakah anda mengalami salah satu dari yang berikut</h2>
+
+<h5> =>Kesulitan bernafas yang parah(Bernafas dengan sangat cepat atau berbicara dalam satu kata) <br/> 
+=>Nyeri dada yang parah <br/>
+=>Sulit untuk bangun <br/>
+=>Merasa kebingungan <br/>
+
+</h5>
+<table>
+    <td> 
+        <tr> 
+        <form method="get" action="./file2.php">
+    <button class="tomboltidak" type="submit" href="file2.php">Tidak</button>
+</form>
+        </tr>
+    </td>
+    <td> 
+        <tr> 
+ 
+        <form method="get" action="./peringatan.php">
+    <button class="tomboliya8" type="submit" href="peringatan.php">Iya</button>
+        </tr>
+    </td>
+</table>
+
+
+
+</div>
+
+</form>
+</body>
+</html>
