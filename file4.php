9d0fe3d07e63671e82c5b2c3e1340d2e5944d80f
revindodewantara1 <63893659+revindodewantara1@users.noreply.github.com>
2020-04-24 13:38:57 +0700

    Add files via upload

---
+++ file4.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>File</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+<div class="quest-box4">
+<img src="avatar2.png" class="avatar">
+<h2>5.Apakah anda memberikan perawatan atau melakukan kontak dekat dengan
+seseorang dengan COVID-19(kemungkinan atau dikonfirmasi)saat mereka sakit
+(batuk, demam,bersin, atau sakit tenggorokan)?</h2>
+
+</h5>
+<table>
+    <td> 
+        <tr> 
+        <form method="get" action="./file5.php">
+    <button class="tomboltidak" type="submit" href="file5.php">Tidak</button>
+</form>
+        </tr>
+    </td>
+    <td> 
+        <tr> 
+ 
+        <form method="get" action="./peringatan.php">
+    <button class="tomboliya3" type="submit" href="peringatan.php">Iya</button>
+        </tr>
+    </td>
+</table>
+</div>
+
+
+
+</form>
+</body>
+</html>
