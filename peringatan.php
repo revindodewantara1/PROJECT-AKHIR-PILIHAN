1dfeb557c79614240902ed794f2aa67335ab5580
revindodewantara1 <63893659+revindodewantara1@users.noreply.github.com>
2020-04-24 13:37:21 +0700

    Add files via upload

---
+++ peringatan.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>File 2</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+<div class="quest-box3">
+<img src="avatar3.png" class="avatar">
+<h2>Gejala-gejala ini membutuhkan perhatian segera. Anda harus segera menelepon Rumah Sakit Terdekat, atau langsung pergi ke instalasi gawat darurat terdekat.</h2>
+
+<form method="get" action="./index.php">
+    <button class="tomboltidak2" type="submit" href="index.php">Apakah anda ingin kembali self check up lagi?</button>
+</form>
+
+<form action="file3.php" method="post">
+</div>
+<br/>
+
+</body>
+</html>
