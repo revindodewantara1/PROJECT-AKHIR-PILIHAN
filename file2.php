99c3b46d8cf706c5304655a4fa88bf71059df027
revindodewantara1 <63893659+revindodewantara1@users.noreply.github.com>
2020-04-24 13:40:05 +0700

    Add files via upload

---
+++ file2.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>File 2</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+<div class="quest-box2">
+<img src="avatar2.png" class="avatar">
+<h2>2.Apakah anda mengalami salah satu dari yang berikut</h2>
+
+<h5> =>Nafas yang pendek saat istirahat <br/> 
+=>Ketidakmampuan untuk berbaring karena kesulitan bernafas<br/>
+=>Kondisi kesehatan kronis yang anda alami dirasakan lebih berat karena kesulitan bernapas<br/>
+
+</h5>
+<table>
+    <td> 
+        <tr> 
+        <form method="get" action="./file1.php">
+    <button class="tomboltidak" type="submit" href="file1.php">Tidak</button>
+</form>
+        </tr>
+    </td>
+    <td> 
+        <tr> 
+ 
+        <form method="get" action="./peringatan.php">
+    <button class="tomboliya" type="submit" href="peringatan.php">Iya</button>
+        </tr>
+    </td>
+</table>
+
+
+<br/>
+</div>
+</body>
+</html>
