06e390b8ad3a362d02f86e0c6c0e017bd18d4e4b
revindodewantara1 <63893659+revindodewantara1@users.noreply.github.com>
2020-04-24 13:37:36 +0700

    Add files via upload

---
+++ kesimpulan.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>File</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+<div class="quest-box3">
+<img src="avatar4.png" class="avatar">
+<h1><br>KESIMPULAN </br></h1>
+<h2>Anda kemungkinan besar tidak terinfeksi oleh COVID-19, Namun anda disarankan untuk tetap tinggal dirumah. Infeksi anda mungkin disebabkan virus selain COVID-19, Oleh karena itu anda tidak perlu dites oleh COVID-19. Meskipun demikian, hindarilah keluar rumah jika memungkinkan. Penyakit anda akan sembuh sendiri dengan cukup makan dan istirahat
+
+Apabila anda mengalami gejala atau mendapatkan informasi baru tentang perjalanan penyakit anda, anda bisa membuka web ini lagi.</h2>
+
+</h5>
+<table>
+    <td> 
+        <tr> 
+<form method="get" action="./index.php">
+    <button class="self" type="submit" href="index.php">Apakah anda ingin kembali self check up lagi?</button>
+</form>
+        </tr>
+    </td>
+
+</table>
+
+</form>
+</div>
+
+
+</form>
+</body>
+</html>
