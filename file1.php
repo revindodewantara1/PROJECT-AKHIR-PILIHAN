1698ee8a18e8b4619e93d02d52bc1a62611c1f8c
revindodewantara1 <63893659+revindodewantara1@users.noreply.github.com>
2020-04-24 13:40:19 +0700

    Add files via upload

---
+++ file1.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>File 2</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+<div class="quest-box">
+    <img src="avatar2.png" class="avatar">
+<h2>3.Apakah anda mengalami salah satu dari yang berikut</h2>
+
+<h5> =>Demam <br/> =>Batuk <br/>
+=>Bersin <br/>
+=>Sakit tenggorokan <br/>
+=>Sulit bernafas <br/>
+
+</h5>
+<table>
+    <td> 
+        <tr> 
+        <form method="get" action="./file3.php">
+    <button class="tomboltidak" type="submit" href="file3.php">Tidak</button>
+</form>
+        </tr>
+    </td>
+    <td> 
+        <tr> 
+ 
+        <form method="get" action="./peringatan.php">
+    <button class="tomboliya8" type="submit" href="peringatan.php">Iya</button>
+        </tr>
+    </td>
+</table>
+
+<br/>
+
+</div>
+</body>
+</html>
